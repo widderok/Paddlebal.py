5d9077ba60d77e187472303f0949ccf856c3de55
Vlad <widderok@gmail.com>
2019-02-27 13:37:24 +0200

    Update Paddleball.py

--- Paddleball.py
+++ Paddleball.py
@@ -2,27 +2,31 @@ from tkinter import *
 import random
 import time
 
+
 class Ball:
-    def __init__(self, canvas, paddle, color):
+    def __init__(self, canvas, paddle, score, color):
         self.canvas = canvas
         self.paddle = paddle
+        self.score = score
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
-        starts = [-3, -2, -1, 1, 2, 3]
+        starts = [-8, -2, -1, 1, 2, 3]
         random.shuffle(starts)
         self.x = starts[0]
         self.y = -3
         self.canvas_height = self.canvas.winfo_height()
-        self.canvas_width = self.canvas.winfo_width()
+        self.canvas_width = self.canvas.winfo_width() 
         self.hit_bottom = False
-
+        
     def hit_paddle(self, pos):
         paddle_pos = self.canvas.coords(self.paddle.id)
         if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
             if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                self.x += self.paddle.x
+                self.score.hit()
                 return True
         return False
-    
+            
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
@@ -45,8 +49,11 @@ class Paddle:
         self.canvas.move(self.id, 200, 300)
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
+        self.started = False
         self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
         self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        self.canvas.bind_all('<Button-1>', self.start_game)
+            
 
     def draw(self):
         self.canvas.move(self.id, self.x, 0)
@@ -58,27 +65,41 @@ class Paddle:
             
     def turn_left(self, evt):
         self.x = -3
-
     def turn_right(self, evt):
         self.x = 3
-        
+    def start_game(self, evt):
+        self.started = True
 
+class Score:
+    def __init__(self, canvas, color):
+        self.score = 0
+        self.canvas = canvas
+        self.id = canvas.create_text(350, 470, font=('Times',20), text= 'Рахунок: ', fill= color)
+        self.id = canvas.create_text(420, 470, font=('Times',20), text=self.score, fill= color)
+
+    def hit(self):
+        self.score += 1
+        self.canvas.itemconfig(self.id, text=self.score)
+            
 tk = Tk()
 tk.title('Game')
 tk.resizable(0, 0)
 tk.wm_attributes('-topmost', 1)
-canvas = Canvas(tk, width = 500, height = 500, bd=0, highlightthickness=0)
+canvas = Canvas(tk, width = 450, height = 500, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
+score = Score(canvas, 'black')
 paddle = Paddle(canvas, 'blue')
-ball = Ball(canvas, paddle, 'red')
+ball = Ball(canvas, paddle, score, 'red')
 
 while 1:
-    if ball.hit_bottom == False:
+    if ball.hit_bottom == False and paddle.started == True:
         ball.draw()
         paddle.draw()
+    if ball.hit_bottom == True:
+        canvas.create_text(250, 200, text = 'К І Н Е Ц Ь', font=('Times',22), fill= 'red')
+        
     tk.update_idletasks()
     tk.update()
     time.sleep(0.03)
-    
